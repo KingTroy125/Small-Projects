017f253f10be239fbbdd88296fe8708cef199c52
Tristan Hendricks <153130773+KingTroy125@users.noreply.github.com>
2024-11-28 17:07:10 +0200

    Add files via upload

---
+++ Text Effect/script.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".type", {
+    strings: ["Programmer.", "Web Developer.", "Front-End Developer. ,Software Developer."],
+    typeSpeed: 80,
+    backSpeed: 60,
+    loop: true,
+});
