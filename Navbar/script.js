735782fd948482fd8ae9f4849f9d26641f106513
Tristan Hendricks <153130773+KingTroy125@users.noreply.github.com>
2024-11-29 22:41:17 +0200

    Add files via upload

---
+++ Navbar/script.js
@@ -0,0 +1,9 @@
+// Add scroll effect
+window.addEventListener('scroll', () => {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+      navbar.classList.add('scrolled');
+    } else {
+      navbar.classList.remove('scrolled');
+    }
+  });
